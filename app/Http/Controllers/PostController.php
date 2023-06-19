bd2355622fc16fbdb58994de5943749e7262d6c7
shinopai <harbor.shinozaki@gmail.com>
2023-06-19 04:35:45 +0000

    記事一覧ページ実装

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return view('post.index');
+        $posts = Post::orderBy('id', 'desc')->paginate(15);
+
+        return view('post.index', compact('posts'));
     }
 }
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,3 +1,22 @@
 <x-app-layout>
-
+  <div class="post">
+    <div class="wrap">
+      <h2 class="post__heading">記事一覧</h2>
+      <ul class="post-list">
+        @forelse ($posts as $post)
+        <a href="" class="contents">
+          <li class="post-list__item">
+            <h3 class="post-list__title">{{ $post->title }}</h3>
+            <p class="post-list__content">
+              {{ $post->content }}
+            </p>
+          </li>
+        </a>
+        @empty
+        <li>記事はありません</li>
+        @endforelse
+      </ul>
+      {{ $posts->links() }}
+    </div>
+  </div>
 </x-app-layout>
